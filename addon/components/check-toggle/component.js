e80976ee76d80993d164fffabf461d40f43b8d00
JasonAllenCorns <jason.a.corns@gmail.com>
2018-07-23 11:51:44 -0700

    codemods from ember-cli-update from @kellyselden

--- addon/components/check-toggle/component.js
+++ addon/components/check-toggle/component.js
@@ -1,21 +1,22 @@
-import Ember from 'ember';
+import { computed } from '@ember/object';
+import Component from '@ember/component';
 import layout from './template';
 
-export default Ember.Component.extend({
+export default Component.extend({
   layout: layout,
   tagName: 'label',
   classNames: 'check-toggle',
   classNameBindings: ['toggleType', 'toggleSize'],
-  toggleType: Ember.computed(function () {
+  toggleType: computed(function () {
     if (this.attrs.type) {
       return "toggle-" + this.attrs.type.toString();
     }
     return "";
   }),
-  toggleName: Ember.computed(function () {
+  toggleName: computed(function () {
     return this.attrs.name || Math.random().toString(36).substring(2, 9);
   }),
-  toggleSize: Ember.computed(function() {
+  toggleSize: computed(function() {
     if (this.attrs.size) {
       return "toggle-" + this.attrs.size.toString();
     }
--- tests/integration/components/check-toggle/component-test.js
+++ tests/integration/components/check-toggle/component-test.js
@@ -1,65 +1,67 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, click, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('check-toggle', 'Integration | Component | check toggle', {
-  integration: true
-});
+module('Integration | Component | check toggle', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('check-toggle renders', function(assert) {
+  test('check-toggle renders', async function(assert) {
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
 
-  this.render(hbs`{{check-toggle}}`);
+    await render(hbs`{{check-toggle}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.dom('*').hasText('');
 
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#check-toggle}}
-      template block text
-    {{/check-toggle}}
-  `);
+    // Template block usage:" + EOL +
+    await render(hbs`
+      {{#check-toggle}}
+        template block text
+      {{/check-toggle}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
-test('check-toggle container has "check-toggle" class', function(assert) {
-  this.render(hbs`
-    {{#check-toggle}}
-      template block text
-    {{/check-toggle}}
-  `);
+    assert.dom('*').hasText('template block text');
+  });
+  test('check-toggle container has "check-toggle" class', async function(assert) {
+    await render(hbs`
+      {{#check-toggle}}
+        template block text
+      {{/check-toggle}}
+    `);
 
-  assert.equal(this.$(':first-child').hasClass('check-toggle'), true, 'has check-toggle class');
-});
-test('check-toggle updates controller property', function (assert) {
-  this.set('toggleProperty', false);
-  assert.equal(this.get('toggleProperty'), false, 'default "toggleProperty" value is false');
+    assert.dom(':first-child').hasClass('check-toggle', 'has check-toggle class');
+  });
+  test('check-toggle updates controller property', async function(assert) {
+    this.set('toggleProperty', false);
+    assert.equal(this.get('toggleProperty'), false, 'default "toggleProperty" value is false');
 
-  this.render(hbs`
-    {{#check-toggle isChecked=toggleProperty}}
-      template block text
-    {{/check-toggle}}
-  `);
-  this.$('label').click();
-  assert.equal(this.get('toggleProperty'), true, 'toggled "toggleProperty" value is true');
-  this.$('label').click();
-  assert.equal(this.get('toggleProperty'), false, 'double-toggled "toggleProperty" value is false');
-});
-test('check-toggle label text renders', function (assert) {
+    await render(hbs`
+      {{#check-toggle isChecked=toggleProperty}}
+        template block text
+      {{/check-toggle}}
+    `);
+    await click('label');
+    assert.equal(this.get('toggleProperty'), true, 'toggled "toggleProperty" value is true');
+    await click('label');
+    assert.equal(this.get('toggleProperty'), false, 'double-toggled "toggleProperty" value is false');
+  });
+  test('check-toggle label text renders', async function(assert) {
 
-  this.render(hbs`
-    {{#check-toggle}}
-      template block text
-    {{/check-toggle}}
-  `);
-  assert.equal(this.$('.labelText').text().trim(), 'template block text', 'has label text');
-});
-test('check-toggle track texts match assignments', function (assert) {
+    await render(hbs`
+      {{#check-toggle}}
+        template block text
+      {{/check-toggle}}
+    `);
+    assert.dom('.labelText').hasText('template block text', 'has label text');
+  });
+  test('check-toggle track texts match assignments', async function(assert) {
 
-  this.render(hbs`
-    {{check-toggle checked-text="itIsChecked" open-text="itIsOpen"}}
-  `);
-  assert.equal(this.$('.toggle-label-before').text().trim(), 'itIsOpen', 'has proper open label text');
-  assert.equal(this.$('.toggle-label-after').text().trim(), 'itIsChecked', 'has proper checked label text');
+    await render(hbs`
+      {{check-toggle checked-text="itIsChecked" open-text="itIsOpen"}}
+    `);
+    assert.dom('.toggle-label-before').hasText('itIsOpen', 'has proper open label text');
+    assert.dom('.toggle-label-after').hasText('itIsChecked', 'has proper checked label text');
+  });
 });
